28044ae72970ee37445d748ecd74b4a7c9b55dc1
James Jung <James Jung>
2017-02-09 15:41:00 -0500

    working unirest call

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CookForMe
 Amazon Alexa Skill for our Junior Design Project
+
+# Node Version
+Lambda function needs 4.3 and above
+
+# Dependencies
+Lambda function uses unirest to do http get calls
+Must do npm install unirest
+
+# Zipping
+Must zip with all lowercase naming
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var done = false;
+var done = true;
+var unirest = require("unirest");
 
 exports.handler = (event, context, callback) => {
     if (!done) {
@@ -7,7 +8,23 @@ exports.handler = (event, context, callback) => {
     }
      else {
         var utter = event.request.intent.slots.ingredients.value;
-        output("Finding Recipes for " + utter, context);
+        // output("Finding Recipes for " + utter, context);
+
+        var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=5&offset=0&query="+utter+"&type=main+course";
+
+        unirest.get(url)
+                .header("X-Mashape-Key", "9xDOL5oTurmshYJT2VeV7g7pxJ5kp1QNpa7jsn2vnL1Al6AcZJ")
+                .header("Accept", "application/json")
+                .end(function (result) {
+                    var recipes = result.body.results;
+                    var speechOutput = '';
+                    if (recipes.length >= 0) {
+                        speechOutput = recipes[0].title;
+                        output(speechOutput, context);
+                    } else {
+                        output("No recipes", context);
+                    }
+                });
     }
 };
 
