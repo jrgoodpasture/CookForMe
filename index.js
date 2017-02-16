ffee051fb13e451dfd9aaa11b0527a79926fef09
Min Je Jung <mjung43@gatech.edu>
2017-02-15 23:34:26 -0500

    Working Alexa SDK Model

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
-var done = true;
+
+'use strict';
+var Alexa = require("alexa-sdk");
 var unirest = require("unirest");
+var appId = 'amzn1.ask.skill.4233738b-7e2a-4fa6-888e-70c9fcd89686';
 
-exports.handler = (event, context, callback) => {
-    if (!done) {
-        output("what would you like to cook today?", context);
-        done = true;
-    } else {
-        var utter = event.request.intent.slots.ingredients.value;
-        // output("Finding Recipes for " + utter, context);
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = appId;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
 
-        var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=5&offset=0&query="+utter+"&type=main+course";
+var handlers = {
+
+    'Find': function () {
+
+        var ingredient = this.event.request.intent.slots.ingredients.value;
+
+        var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=5&offset=0&query=" + ingredient + "&type=main+course";       
+
+        this.emit('Calling', this, url);
+    },
+    'Calling': function(that, url) {
+
+        console.log("***** Find ***** " + url);
+        
+        console.log("***** Unirest Call *****");
 
         unirest.get(url)
                 .header("X-Mashape-Key", "9xDOL5oTurmshYJT2VeV7g7pxJ5kp1QNpa7jsn2vnL1Al6AcZJ")
                 .header("Accept", "application/json")
                 .end(function (result) {
+                    console.log(result.status, result.headers, result.body);
+                          
                     var recipes = result.body.results;
+                    
                     var speechOutput = '';
+                    
                     if (recipes.length >= 0) {
                         for (var i = 0; i < recipes.length && i < 5; i++) {
-                            speechOutput += (" Number " + (i + 1) + " " + recipes[i].title + ".");
+                            speechOutput += (" Number " + (i + 1) + ": " + recipes[i].title + ".");
                         }
-                        output(speechOutput, context);
                     } else {
-                        output("No recipes", context);
+                        speechOutput = "No recipes found";
                     }
+                    console.log(speechOutput);
+                    that.emit(':tell', speechOutput);
                 });
     }
 };
-
-function output( text, context ) {
-
-    var response = {
-        outputSpeech: {
-            type: "PlainText",
-            text: text
-        },
-        card: {
-            type: "Simple",
-            title: "CookForMe",
-            content: text
-        },
-        shouldEndSession: false
-    };
-    
-    context.succeed( { response: response } );
-}
