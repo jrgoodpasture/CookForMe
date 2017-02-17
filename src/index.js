9bea7df3fc63f15055f1547894c4be433ef5f221
Min Je Jung <mjung43@gatech.edu>
2017-02-16 20:26:52 -0500

    working sprint 2 code

--- src/index.js
+++ src/index.js
@@ -35,15 +35,68 @@ var handlers = {
                     
                     var speechOutput = '';
                     
-                    if (recipes.length >= 0) {
-                        for (var i = 0; i < recipes.length && i < 5; i++) {
+                    if (result.body.totalResults > 0) {
+                        for (var i = 0; i < recipes.length; i++) {
                             speechOutput += (" Number " + (i + 1) + ": " + recipes[i].title + ".");
                         }
+                        speechOutput = speechOutput.replace("&", "and");
                     } else {
                         speechOutput = "No recipes found";
                     }
                     console.log(speechOutput);
                     current.emit(':tell', speechOutput);
                 });
-    }
+    },
+	'GetInstruction': function() {
+		var current = this;
+		var name = current.event.request.intent.slots.food.value;
+		console.log("*************" + name);
+
+    	var key = "9xDOL5oTurmshYJT2VeV7g7pxJ5kp1QNpa7jsn2vnL1Al6AcZJ";
+    	var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=1&offset=0&query=" + name + "&type=main+course";
+    	// //Getting the recipe id
+    	unirest.get(url)
+                .header("X-Mashape-Key", "9xDOL5oTurmshYJT2VeV7g7pxJ5kp1QNpa7jsn2vnL1Al6AcZJ")
+                .header("Accept", "application/json")
+                .end(function (result) {
+                    console.log(result.status, result.headers, result.body);
+                          
+                    var recipes = result.body.results;
+                    var id = 0;
+                                        
+                    if (result.body.totalResults > 0) {
+                    	id = recipes[0].id;
+
+						var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/" + id + "/analyzedInstructions?stepBreakdown=true";
+                    	unirest.get(url)
+				                .header("X-Mashape-Key", "9xDOL5oTurmshYJT2VeV7g7pxJ5kp1QNpa7jsn2vnL1Al6AcZJ")
+				                .header("Accept", "application/json")
+				                .end(function (result) {
+				                	console.log(result.status, result.headers, result.body);
+				                	if (result.body.length > 0) {
+				                		var instruction = result.body[0].steps;
+
+					                	var speechOutput = '';
+
+					                	for (var i = 0; i < instruction.length; i++) {
+					                		speechOutput += (" Step " + (i + 1) + ": " + instruction[i].step + ".");
+					                	}
+
+					                	speechOutput = speechOutput.replace("(PRINTABLE)", "");
+
+					                	current.emit(':tell', speechOutput);
+				                	} else {
+				                		current.emit(':ask', "Could not find the recipe you wanted.", "What else would you like to cook today?");
+				                	}
+				                });
+
+                    } else {
+                        current.emit(':ask', "Could not find the recipe you wanted.", "What else would you like to cook today?");
+                    }
+                });
+    },
+    'Unhandled': function () {
+		var message = "What would you like to cook today?";
+		this.emit(':ask', message);
+	}
 };
